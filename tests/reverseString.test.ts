24a1fa693ec27995d01c171c4e7b904e269bbdc7
LalaineHines <lalainehines@gmail.com>
2025-08-17 15:47:19 -0700

    added reverse string func test

---
+++ tests/reverseString.test.ts
@@ -0,0 +1,23 @@
+import { reverseString } from '../src/reverseString';
+
+describe.skip('reverseString', () => {
+  it('reverses a normal string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+  });
+
+  it('reverses empty string', () => {
+    expect(reverseString('')).toBe('');
+  });
+
+  it('reverses palindrome unchanged', () => {
+    expect(reverseString('madam')).toBe('madam');
+  });
+
+  it('reverses string with spaces and punctuation', () => {
+    expect(reverseString('hello world!')).toBe('!dlrow olleh');
+  });
+
+  it('reverses string with emojis', () => {
+    expect(reverseString('🙂🙃')).toBe('🙃🙂');
+  });
+});
